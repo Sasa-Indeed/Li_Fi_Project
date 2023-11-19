804b3e5fffdda67f77abe2b9e609ed05abacd1f6
Ahmed Mostafa <108436896+Sasa-Indeed@users.noreply.github.com>
2023-11-19 23:50:17 +0200

    Delete Code directory

--- Code/INC/bitwiseOperations.h
+++
@@ -1,9 +0,0 @@
-#ifndef BITWISEOPERATIONS_H_
-#define BITWISEOPERATIONS_H_
-
-#define CLEAR_BIT(reg,bit)		(reg &= ~(1<<bit))
-#define SET_BIT(reg,bit)		(reg |= (1<<bit))
-#define TOGGLE_BIT(reg,bit)		(reg ^= (1<<bit))
-#define READ_BIT(reg,bit)		((reg>>bit)&1)
-
-#endif /* BITWISE_OPERATIONS_H_ */
